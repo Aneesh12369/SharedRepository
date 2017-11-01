2f9558d3543b57f77677bdb78aecef443b2dd615
aneesh12369 <aneeshreddy6@gmail.com>
2017-11-01 16:47:32 +0530

    Sorting arraylist according to date

--- SortingWithDate/src/com/practice/PracticeClass.java
+++ SortingWithDate/src/com/practice/PracticeClass.java
@@ -2,27 +2,28 @@ package com.practice;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class PracticeClass {
 
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
 		ArrayList<TableRecords> tableRecordsList = new ArrayList<TableRecords>();
-		
-		for(int i=0 ;i<20;i++){
+		Random random = new Random();
+		for (int i = 0; i < 20; i++) {
 			TableRecords tableRecords = new TableRecords();
 			tableRecords.setSno(i);
-			tableRecords.setName("a"+i);
+			tableRecords.setName("a" + i);
 			tableRecords.setCompany("Pyro");
 			tableRecords.setProject("CTOPUP");
-			
+			tableRecords.setDateString("2014/10/29 18:10:4" + random.nextInt(9));
 			tableRecordsList.add(tableRecords);
 		}
-		MyComparator comparator = new MyComparator();
-		Collections.sort(tableRecordsList,comparator);
 		
-		for(int i=0;i<10;i++){
-			System.out.println(tableRecordsList.get(i).toString());
+		System.out.println(tableRecordsList);
+		Collections.sort(tableRecordsList, new MyComparator());
+		System.out.println("******************************");
+		for (TableRecords record:tableRecordsList) {
+			System.out.println(record);
 		}
 	}
 }
